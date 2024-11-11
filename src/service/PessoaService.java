df5223d20774fa4bfd59362c1ddf501eabe1ae56
Mailson <mailsonestudo48@gmail.com>
2024-11-11 20:18:55 -0300

    Atualizado

--- src/service/PessoaService.java
+++ src/service/PessoaService.java
@@ -1,4 +1,25 @@
 package service;
 
+import model.Pessoa;
+
+import java.util.ArrayList;
+
 public class PessoaService {
+    private ArrayList<Pessoa>pessoas = new ArrayList<>();
+
+
+    public void cadastrarPessoa(Pessoa p){
+        pessoas.add(p);
+        System.out.println("cadastrada com Sucesso!");
+    }
+    public ArrayList<Pessoa> getPessoas() {
+        return pessoas;
+    }
+
+    public void setPessoas(ArrayList<Pessoa> pessoas) {
+        this.pessoas = pessoas;
+    }
+    public ArrayList<Pessoa>listar(){
+        return pessoas;
+    }
 }
